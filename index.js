baf1e30dcc6371d7a02706399c7155394769106b
noahfranco <noahfra@gmail.com>
2019-10-03 18:32:44 -0400

    add a few things

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const express = require("express");
 const server = express(); 
 
 server.get("/", (req, res) => {
-    res.send("Noah's API made from Scratch")
+    res.status(200).json({ message: "Noah's API made from Scratch"})
 })
 
-module.exports = server; 
+// module.exports = server; 
 
-const server = require("./server.js")
+// const server = require("./server.js")
 
 // for heroku we need a dynamic port 
 // we can read the PORT value added by heroku to the environment 
