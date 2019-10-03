c700d98397b48e0308d2e5f998d14381621a700f
noahfranco <noahfra@gmail.com>
2019-10-03 18:28:05 -0400

    changed a few things

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 require("dotenv").config(); 
 
+const express = require("express"); 
+
+const server = express(); 
+
+server.get("/", (req, res) => {
+    res.send("Noah's API made from Scratch")
+})
+
+module.exports = server; 
+
 const server = require("./server.js")
 
 // for heroku we need a dynamic port 
